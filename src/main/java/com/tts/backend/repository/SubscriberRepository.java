b17f558ed6c80d389259096f7f77d3af17aa39ed
Lizziebeth76 <lizziebeth76@gmail.com>
2021-02-24 17:04:06 -0600

    first commit

---
+++ src/main/java/com/tts/backend/repository/SubscriberRepository.java
@@ -0,0 +1,9 @@
+package com.tts.backend.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.tts.backend.model.Subscriber;
+
+public interface SubscriberRepository extends CrudRepository<Subscriber, Long>{
+
+}
